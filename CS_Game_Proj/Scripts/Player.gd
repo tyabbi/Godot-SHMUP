a538e71c4f9e89e2fee8f2c4d228d641d8aadcf8
April Trinh <56134712+soupbara@users.noreply.github.com>
2021-04-20 21:30:36 -0700

    Added Player Sprite & Boss
    - base code controls
    - boss can fire bullets

--- CS_Game_Proj/Scripts/Player.gd
+++ CS_Game_Proj/Scripts/Player.gd
@@ -1,4 +1,13 @@
-extends KinematicBody2D
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
 
 func _physics_process(delta):
 	var move_vec = Vector2()
@@ -11,6 +20,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_up"):
 		move_vec.y += 1
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("shoot"):
 		shoot()
