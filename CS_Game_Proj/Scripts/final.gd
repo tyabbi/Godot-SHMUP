9720f569fb79de98ee93869249f813f0c85c8ee5
April Trinh <56134712+soupbara@users.noreply.github.com>
2021-05-13 23:20:37 -0700

    added ending scene

--- CS_Game_Proj/Scripts/final.gd
+++ CS_Game_Proj/Scripts/final.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for button in $menu/Buttons.get_children():
+	for button in $VBoxContainer/Buttons.get_children():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
 
 func _on_button_pressed(scene_to_load):
